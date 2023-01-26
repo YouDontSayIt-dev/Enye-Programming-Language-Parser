b10c10421f75402d1875f37308d1529a7dfb77b0
blancojames88 <blancojames88@gmail.com>
2023-01-27 07:14:26 +0800

    added comments

--- try.c
+++ try.c
@@ -81,9 +81,12 @@ int main() {
                     }
       
        fclose(inputfptr);    
+       //run lexical analysis
        lexer(lexerData);
        fclose(outputfptr);
+       //open file for parsing
        file = fopen(symbol_filepath, "r");
+       //run parser
        parser();
        printf("%d",errorFlag);
         fclose(file);
@@ -95,13 +98,18 @@ int main() {
 }
 }
 
+
+//recursive function to parse declarative statements
+//will receive a token from the lexer
+//then parse it base on the grammar
 void parse_declaration() {
-    data_type();
-    parseToken();
-    assignment();
+    data_type(); //parse data type
+    parseToken(); //get next token
+    assignment(); //parse assignment statement
     //parse_declarator();
 }
 
+//parses data type
 void data_type(){
     if (strcmp(currentToken, "INT_KW") == 0){
         printf("Found int declaration\n");
@@ -122,6 +130,10 @@ void data_type(){
 }
 
 //ASSIGNMENT STATEMENTS
+
+//a function that checks if the current token is the expected token
+//if not, it will print an error message
+//if it is an invalide, it will print an error message
 void match(char* expected) {
     if (strcmp("INVALID_IDENTIFIER", expected) == 0) {
         printf("Error: unexpected token %s at line: %d\n", currentToken, lineNo);
@@ -133,10 +145,17 @@ void match(char* expected) {
     }
 }
 
+//a recursive function that parses assignment statements
+//will receive a token from the lexer
+//then parse it base on the grammar
+//it will match the given token to the expected token using the match function
 void assignment() {
-    
     match(currentToken);
+    //if the current token is the expected token, it will get the next token
+    //this should get the identifier
     parseToken();
+    //if the token is a semicolon, it will match the token to the expected token
+    //and will print a success message if the token is the expected token which is a semi-colon
     if(strcmp(currentToken, "SEMI_COLON") == 0){
         match("SEMI_COLON");                
         if(errorFlag == 0){
@@ -145,10 +164,17 @@ void assignment() {
             printf("Parsing assignment statement failed\n");
         }
     }else if(strcmp(currentToken, "ASS_OPR") == 0){
-        
+        //if the token is not a semi-colon, it will match the token to the expected token
+        //the expected token is an assignment operator
         match("ASS_OPR");
+        //if the token is the expected token, it will get the next token
         parseToken();
+        //if the token is a semi-colon, it will call the recursive function expression()
         expression();
+        //once the expression is parsed, it will match the token to the expected token
+        //the expected token is a semi-colon
+        //if the token is the expected token, it will print a success message
+        //if not, it will print an error message
         match("SEMI_COLON");
         if(errorFlag == 0){
             printf("Parsing assignment statement success\n");
@@ -158,8 +184,13 @@ void assignment() {
     }
 }
 
+//a recursive function that parses expressions
 void expression() {
+    //will first call the recursive function term()
     term();
+    //if term is parsed, it will check if the current token is an add or subtract operator or a comma
+    //if it is, it will match the token to the expected token
+    //and will call the recursive function term()
     while (strcmp(currentToken, "ADD_OPR") == 0 || strcmp(currentToken, "SUBT_OPR") == 0 || strcmp(currentToken, "COMMA") == 0) {
         match(currentToken);
         parseToken();
@@ -167,8 +198,13 @@ void expression() {
     }
 }
 
+//a recursive function that parses terms
 void term() {
+    //will first call the recursive function power()
     power();
+    //if power is parsed, it will check if the current token is a multiply, divide, modulus or integer divide operator
+    //if it is, it will match the token to the expected token
+    //and will call the recursive function power()
     while (strcmp(currentToken, "MULT_OPR") == 0 || strcmp(currentToken, "DIV_OPR") == 0 || 
     strcmp(currentToken, "MOD_OPR") == 0 || strcmp(currentToken, "INT_DIV_OPR") == 0) {
         match(currentToken);
@@ -177,10 +213,13 @@ void term() {
     }
 }
 
-
-
+//a recursive function that parses powers
 void power(){
+    //will first call the recursive function factor()
     factor();
+    //if factor is parsed, it will check if the current token is an exponent operator
+    //if it is, it will match the token to the expected token
+    //and will call the recursive function factor()
     while (strcmp(currentToken, "EXP_OPR") == 0) {
         match(currentToken);
         parseToken();
@@ -188,8 +227,13 @@ void power(){
     }
 }
 
+//a recursive function that parses factors
 void factor() {
-    
+    //if the current token is an real number,identifier, or string literal- it will match the token to the expected token
+    //and will get the next token
+    //if the token is an string, it will loop while the token is still a string
+    //if the token is an invalid identifier, it will print an error message
+    //if the token is not any of the expected tokens, it will print an error message
     if (strcmp(currentToken, "REAL_NUMBER") == 0 || strcmp(currentToken, "INTEGER") == 0) {
         match(currentToken);
         parseToken();
@@ -207,6 +251,10 @@ void factor() {
             parseToken();
         }
     }else if (strcmp(currentToken, "LEFT_PARENTHESIS") == 0) {
+        //if the token is a left parenthesis, it will match the token to the expected token
+        //and will call the recursive function expression()
+        //and will match the right parenthesis to the expected token
+        //and will get the next token
         match("LEFT_PARENTHESIS");
         parseToken();
         expression();
@@ -220,15 +268,26 @@ void factor() {
 
 
 //OUTPUT STATEMENT
+
+//this function parses output statements
 void output_statement() {
+    //if the token is a print keyword, it will match the token to the expected token
     match("PRINTF_KW");
+    //and will get the next token
     parseToken();
+    //if the token is a left parenthesis, it will match the token to the expected token
     match("LEFT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will call the recursive function expression()
     expression();
+    //if the token is a right parenthesis, it will match the token to the expected token
     match("RIGHT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //if the token is a semi-colon, it will match the token to the expected token
     match("SEMI_COLON");
+    //if the error flag is 0, it will print a success message
     if(errorFlag == 0){
         printf("Parsing output statement success\n");
     }else{
@@ -237,15 +296,26 @@ void output_statement() {
 }
 
 //INPUT STATEMENT
+
+//this function parses input statements
 void input_statement() {
+    //if the token is a scan keyword, it will match the token to the expected token
     match("SCANF_KW");
+    //and will get the next token
     parseToken();
+    //if the token is a left parenthesis, it will match the token to the expected token
     match("LEFT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then call the recursive function expression()
     expression();
+    //if the token is a right parenthesis, it will match the token to the expected token
     match("RIGHT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //if the token is a semi-colon, it will match the token to the expected token
     match("SEMI_COLON");
+    //if the error flag is 0, it will print a success message
     if(errorFlag == 0){
         printf("Parsing input statement success\n");
     }else{
@@ -255,28 +325,52 @@ void input_statement() {
 
 
 // ITERATIVE STATEMENT
+
+//this function parses iterative statements
 void iterative_statement(){ 
+    //if the token is a for keyword, it will match the token to the expected token
     match("FOR_KW");
+    //and will get the next token
     parseToken();
+    //if the token is a left parenthesis, it will match the token to the expected token
     match("LEFT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then call the recursive function assignment()
     assignment();
+    //and will get the next token
     parseToken();
+    //it will then call the function cond()
     cond();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("SEMI_COLON");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("IDENTIFIER");
+    //and will get the next token
     parseToken();
+    //it will then call the function unary_opr()
     unary_opr();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("LEFT_CURLY_BRACES");
+    //and will get the next token
     parseToken();
+    //it will then call the function stmt()
     stmt();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_CURLY_BRACES");
+    //if the error flag is 0, it will print a success message
     if(errorFlag == 0){
         printf("Parsing iterative statement success\n");
     }else{
@@ -284,6 +378,7 @@ void iterative_statement(){
     }
 }
 
+//a function that matches the current token to the expected token
 void rel_opr(){
     if (strcmp(currentToken, "GREAT_OPR") == 0 ){
         match(currentToken);
@@ -308,6 +403,7 @@ void rel_opr(){
     }
 }
 
+//a function that matches the current token to the expected token
 void log_opr(){
     if (strcmp(currentToken, "AND_OPR") == 0 ){
         match(currentToken);
@@ -320,14 +416,21 @@ void log_opr(){
     }
 }
 
+//a function that parses conditional statements
 void cond(){
+    //it will call the function variable()
+    //and will get the next token
+    //it will then call the function rel_opr()
+    //and will get the next token
+    //it will then call the function variable()
         variable();
         parseToken();
         rel_opr();
         parseToken();
         variable();
-    }
+}
 
+//a function that matches the current token to the expected token
 void constants(){
     if (strcmp(currentToken, "INTEGER") == 0){
         match(currentToken);
@@ -340,6 +443,8 @@ void constants(){
     }
 }
 
+//a function that matches the current token to the expected token
+//if the token is not matched as an identifier, then it is a constant
 void variable(){
     if (strcmp(currentToken, "IDENTIFIER") == 0){
         match(currentToken);
@@ -349,6 +454,8 @@ void variable(){
         }
 }
 
+//a function that matches the current token to the expected token
+//if a token is not matched as an increment operator, then it is a decrement operator
 void unary_opr(){
     if (strcmp(currentToken, "INC_OPR") == 0 ){
         match(currentToken);
@@ -358,14 +465,23 @@ void unary_opr(){
     }
 }
 
+//a recursive function that parses else statements
 void else_stmt(){
+    //if the token is an else keyword, it will match the token to the expected token
     match("ELSE_KW");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("LEFT_CURLY_BRACES");
+    //and will get the next token
     parseToken();
+    //it will then call the function stmt()
     stmt();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_CURLY_BRACES");
+    //if the error flag is 0, it will print a success message
     if(errorFlag == 0){
         printf("Parsing else statement success\n");
     }else{
@@ -373,22 +489,39 @@ void else_stmt(){
     }
 }
 
+//a recursive function that parses else if statements
 void else_if_stmt(){
+    //if the token is an else keyword, it will match the token to the expected token
     match("ELSE_KW");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("IF_KW");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("LEFT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then call the function cond()
     cond();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("LEFT_CURLY_BRACES");
+    //and will get the next token
     parseToken();
+    //it will then call the function stmt()
     stmt();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_CURLY_BRACES");
+    //if the error flag is 0, it will print a success message
     if(errorFlag == 0){
         printf("Parsing else if statement success\n");
     }else{
@@ -397,20 +530,36 @@ void else_if_stmt(){
 }
 
 // Conditional Statements
+
+//a function that parses conditional statements
 void conditional_stmt(){
+    //if the token is an if keyword, it will match the token to the expected token
     match("IF_KW");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("LEFT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then call the function cond()
     cond();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_PARENTHESIS");
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("LEFT_CURLY_BRACES");
+    //and will get the next token
     parseToken();
+    //it will then call the function stmt()
     stmt();
+    //and will get the next token
     parseToken();
+    //it will then match the token to the expected token
     match("RIGHT_CURLY_BRACES");
+    //if the error flag is 0, it will print a success message
     if(errorFlag == 0){
         printf("Parsing conditional statement success\n");
     }else{
@@ -419,7 +568,9 @@ void conditional_stmt(){
 }
 
 // START OF GRAMMAR RULE
+//start of the parser
 void parser(){
+    //do the following while the statement flag is greater than or equal to the line number
     do{
          parseToken();
          stmts();
@@ -433,11 +584,17 @@ void parser(){
 }
 
 void stmts(){
+    //error flag that increments if there is an error
     errorFlag=0;
+    //call the function stmt()
     stmt();
     
 }
 
+
+//a function that parses statements
+//it will call the appropriate function depending on the token
+//if the token is not matched, it will print an error message
 void stmt(){
       if(strcmp(currentToken, "INT_KW") == 0 || strcmp(currentToken, "FLOAT_KW") == 0 
       || strcmp(currentToken, "CHAR_KW") == 0 || strcmp(currentToken, "STRING_KW") == 0){
@@ -471,10 +628,13 @@ void stmt(){
         }
 }
 
+//a function that gets the next token
 void parseToken(){
     char ch = fgetc(file);
     int index = 0;
 
+    //if ch is a \n, it will increment the line number
+    //while ch is a \n, it will get the next character
     if(ch == '\n'){
         lineNo++;
         ch = fgetc(file);
